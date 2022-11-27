8c8063b542267ef4beedd56363dff665fcd6e5d5
Alex Filip <alexfilippaul@gmail.com>
2022-11-27 17:36:46 +0200

    ultima cifra a unui numar citit

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ultima cifra a unui nr introdus de la tastatura
+            int a;
+            Console.WriteLine("Introduceti un numar: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ultima cifra a n umarului introdus este: " + a%10);
+
+        }
+    }
+}
